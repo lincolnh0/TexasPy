1ed2f18656b60e2adfb4da821a8688e56241dedd
lincolnh0 <holincolnly@gmail.com>
2020-03-27 00:11:14 +0000

    Folding and sidepot progress + manual input for base player class

--- player.py
+++ player.py
@@ -11,20 +11,26 @@ class Player(object):
     def __repr__(self):
         return repr((self.name, self.hand, self.chips))
 
+    def setId(self, id):
+        self.id = id
+
     def setHand(self, hand):
         self.hand = hand
 
-    def setTable(self, tableCards):
-        self.tableCards = tableCards
-
-    def getAction(self, toCall):
+    def getAction(self, toCall, tableCards):
         """ 
         Returns integer:
         <: fold
         0: check
         >: call/bet/raise
         """
-        pass
+        print('\n%s\'s turn -- %d to call' % (self.name, toCall))
+        print('Hand: ' , poker.returnCardStringShort(self.hand))
+        self.action = input('Please enter your value: ')
+        return int(self.action), int(self.action == self.chips)
+
+    def setAction(self, value):
+        self.action = value
 
     def setRecord(self, action):
         pass
--- table.py
+++ table.py
@@ -1,7 +1,5 @@
 from random import shuffle
-from collections import deque
-import numpy as np
-import random
+from collections import defaultdict, deque
 
 import player, poker
 
@@ -9,12 +7,18 @@ class GameObject(object):
 
     def __init__(self, players, blinds, ante=0):
         self.players = {id:player for id, player in enumerate(players)}
+        self.blinds = blinds
         self.dealer = 0
+
+    def run(self, count):
+        for i in range(count):
+            self.setup()
+            self.playHand()
 
     def setup(self):
         ''' Performs initial set up of a new hand. '''
         # Update dealer, collect blinds / antes
-        self.dealer = (self.dealer + 1) % len(self.players)
+        # self.dealer = (self.dealer + 1) % len(self.players)
 
         # Clear player hands, community cards and pot
         self.table = []
@@ -22,47 +26,124 @@ class GameObject(object):
 
         # Shuffle cards
         self.deck = list(range(52))
-        random.shuffle(self.deck)
+        shuffle(self.deck)
 
         # Deal two cards to each player
         for id, player in self.players.items():
-            startIndex = 2 * id
-            player.setHand([self.deck[startIndex: startIndex+ 2]])
+            start_index = 2 * id
+            player.setHand(self.deck[start_index: start_index+ 2])
         
         # Remove dealt cards from deck
         self.deck = self.deck[len(self.players) * 2:]
 
     def dealTable(self):
-        ''' Store the first 3, 4 or 5 cards of the remaining deck as community cards '''
+        ''' Store the first n cards of the remaining deck as community cards '''
         if len(self.table) == 0:
             self.table = self.deck[:3]
-        elif len(self.table) < 5:
-            self.table = self.deck[:(len(self.table))]
+        else:
+            self.table = self.deck[:len(self.table) + 1]
         
 
-    def run(self):
+    def createSidePots(self, round_bet, in_play):
+        # Get lowest number x of round_bet[in_play_index]
+        # Create pot with (len(in_play) * x , in_play)
+        # Remove in_play_index from in_play
+        # Repeat until all round_bet values are the same
+        # Return all sidepots
+
+
+    
+
+        leftover_bets = sum([round_bet[x] for x in round_bet.keys if x not in in_play])
+        sidepot = []
+        # Get all the bets of the in play players
+        bets = [round_bet[x] for x in in_play]
+
+        last_pot_bet = min(bets)
+        pot = last_pot_bet * len(bets)
+        bets = [x - last_pot_bet for x in bets if x > last_pot_bet]
+
+
+        # While their bets are not equal
+        while (len(set(bets)) != 1):
+            # Create sidepot for the smallest bet * no. of still in play players, eligible for the same group of players
+            sidepot.append((min(bets) * len(bets), [x for x in in_play if round_bet[x] >= min(bets)]))
+
+            # Update the remainging bets to be subtracted from the sidepot that was just generated
+            # And add them to the next sidepot generation only if they are greater than the one just now
+            bets = [round_bet[x] - min(bets) for x in in_play if round_bet[x] > min(bets)]
+
+        if (len(sidepot) == 0):
+            return [(sum(bets), in_play)]
+
+        main_pot, main_pot_players = sidepot[0]
+        sidepot[0] = (leftover_bets + main_pot, main_pot_players)
+
+    def playHand(self):
         
         # Setup players in play and rotate table
-        inPlay = deque(self.players.keys())
-        inPlay.rotate(self.dealer)
+        # Ids of in-play players
+        in_play = deque(self.players.keys())
+        in_play.rotate(-self.dealer)
+
+        small_blind = in_play[-2]
+        big_blind = in_play[-1]
+
+        # Set up round bet records
+        round_bet = defaultdict(int)
+        round_bet[small_blind] = self.blinds / 2
+        round_bet[big_blind] = self.blinds
+        
+
         # Keep track of main and sidepots
-        self.pot = [(0, inPlay)]
+        self.pot = [(0, in_play)]
+        current_pot = 0
 
-        toCall = 0 
-        currentPot = 0
-
-        while len(self.table) <= 5:
-            folded = []
-
-            for id in inPlay:
-                action = self.players[id].getAction(toCall)
-                if action == -1: folded.append(id)
-                
-
+        # Starts hand
+        for i in range(4):
+            print('Table cards: ' , poker.returnCardStringShort(self.table))
             
+
+            current_player_id = in_play[0]
+            last_bet_player_id = in_play[-1]
+
+            while current_player_id != last_bet_player_id or round_bet[last_bet_player_id] == self.blinds:
+                
+                # TODO: skip player's turn if they have zero chips but are still in play i.e. have already all in
+                if round_bet[current_player_id] != 0 and self.players[current_player_id].chips == 0:
+                    current_player_id = in_play[(in_play.index(current_player_id) + 1) % len(in_play)]
+                    continue
+
+                toCall = round_bet[last_bet_player_id] - round_bet[current_player_id]
+                action, all_in = self.players[current_player_id].getAction(toCall, self.table)
+
+                # Move pointer when current player does not fold and not an all in
+                next_pointer = in_play.index(current_player_id) + int(action != -1)
+
+                if action == -1 :
+                    in_play.remove(current_player_id)
+                else:
+                    current_pot += action
+                    round_bet[current_player_id] += action
+
+                    # If raise, set this bet round to end with current player.
+                    if round_bet[current_player_id] > round_bet[last_bet_player_id]:
+                        last_bet_player_id = current_player_id
+
+                # Special case for big blinds to check.
+                if current_player_id == last_bet_player_id and round_bet[current_player_id] == self.blinds: break
+
+                current_player_id = in_play[next_pointer % len(in_play)]
+                
+            
+            if len(in_play) == 1: break
             # Update eligible players for current pot
-            inPlay = [id for id in inPlay if id not in folded]
-            self.pot[-1] = (currentPot, inPlay)
+
+            self.createSidePots(round_bet, in_play)
+
+            self.pot[-1] = (current_pot, in_play)
+
+            print('Round %d ended\n' % (i+1))
 
             # Deals table card once betting is finished
             self.dealTable()
